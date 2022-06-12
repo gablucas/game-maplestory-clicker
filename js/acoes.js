508ca440e06a3329bc6e7d971914d29b1f39abe1
Gabriel Lucas <gabthedeveloper@gmail.com>
2022-06-12 16:29:40 -0300

    Refeito todo o menu, ajustado o tamanho da tela para dispositivos moveis, entre outras modificações

--- js/acoes.js
+++ js/acoes.js
@@ -1,22 +1,33 @@
-// Define o index a percorrer
+// Define o index a percorrer pelo array de monstros
 let indexMonstro = 0;
 
 // Ataque
 let hpReduzir = 0;
 
-// Ganhar XP ao clicar no monstro
+/** AÇÃO AO ATACAR O MONSTRO */
 function ataqueMonstro() {
+
+  // Calcula a porcentagem de HP do monstro de acordo com o tamanho do container
   let hpPorcentagem = lifeMonstro.clientWidth / (monstros[indexMonstro].hpMonstro / ataque);
   hpMonstro -= ataque;
-  hpReduzir += hpPorcentagem;
-  lifeMonstro.style.boxShadow = `inset -${hpReduzir}px 0 black`;
 
+  // Incrementa a porcentagem acima e reduz graficamente a vida do monstro
+  hpReduzir += hpPorcentagem;
+  lifeMonstro.style.boxShadow = `inset -${hpReduzir}px 0 rgba(0, 0, 0, 0.6)`;
+
+  // Mostrar progresso do XP na barra
+  // Faz o calculo da porcetagem de acordo com o tamanho do container, foi preciso arredondar o valor, pois alguns numeros quebrados causavam bug.
+  let xpPorcentagem = Math.round(xpAtual * barraXP.clientWidth / xpProximoLevel)
+  barraXP.style.boxShadow = `inset ${xpPorcentagem}px 0 yellow`;
+  barraXP.innerText = xpAtual +" / "+ xpProximoLevel
+
+  /** AÇÃO QUANDO O MONSTRO MORRE */
   if(hpMonstro <= 0) {
 
-  // Adicionar XP
+  // Adiciona XP
   xpAtual += xpMonstro;
 
-  // Ações após subir de level
+  // Ação quando sobre de nivel
   if(xpAtual >= xpProximoLevel) {
     ++level;
     xpAtual -= xpProximoLevel;
@@ -25,24 +36,16 @@ function ataqueMonstro() {
   }
 
   // Verifica se o proximo monstro tem o mesmo level que o persosagem
-  if(indexMonstro + 1 < monstros.length) {
-    if(level >= monstros[indexMonstro + 1].levelMonstro) {
-      setaDireita.setAttribute('src', 'img/buttons/seta_direita.png');
-      setaDireita.setAttribute('onclick', 'proximoMonstro()');
-    }
+  if(indexMonstro + 1 < monstros.length && level >= monstros[indexMonstro + 1].levelMonstro) {
+    setaDireita.setAttribute('src', 'img/buttons/seta_direita.png');
+    setaDireita.setAttribute('onclick', 'proximoMonstro()');
   }
-
+  
   // Restaurar HP do monstro
   nomeMonstro.style.boxShadow = `inset 0px 0px white`;
   hpMonstro = monstros[indexMonstro].hpMonstro;
   hpReduzir = 0;
-}
-
-  // Mostrar progresso do XP na barra
-  // Faz o calculo da porcetagem referente ao tamanho da div, foi preciso arredondar o valor, pois alguns numeros quebrados causavam bug
-  let xpPorcentagem = Math.round(xpAtual * barraXP.clientWidth / xpProximoLevel)
-  barraXP.style.boxShadow = `inset ${xpPorcentagem}px 0 yellow`;
-  barraXP.innerText = xpAtual +" / "+ xpProximoLevel
+  }
 }
 
   monstro.addEventListener('click', ataqueMonstro)
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Background
+const background = document.querySelector('.js-game');
+
 // Level
 let level = 100;
 let xpProximoLevel = 10;
@@ -14,6 +17,18 @@ barraXP.innerHTML = xpAtual +" / "+ xpProximoLevel
 // Ataque
 let ataque = 1;
 
-// Background
-const background = document.querySelector('.js-game');
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+
 
